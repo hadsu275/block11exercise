59c473652013cc0d69699564a04481e8bff56444
niklasbroberg <niklas.broberg@gmail.com>
2023-02-14 16:41:08 +0100

    Re-add dummy files, forcing the presence of the folders.

---
+++ DIT953/controller/DummyController.java
@@ -0,0 +1,5 @@
+package DIT953.controller;
+
+// This file only exists so Git won't skip the folder. Don't use it.
+public class DummyController {
+}
---
+++ DIT953/view2d/DummyView.java
@@ -0,0 +1,5 @@
+package DIT953.view2d;
+
+// This file only exists so Git won't skip the folder. Don't use it.
+public class DummyView {
+}
